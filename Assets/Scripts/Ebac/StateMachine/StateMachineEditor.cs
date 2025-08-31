1ad77fec6ff9fecc248507beaf4540f8ca8d309b
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-08-31 19:45:30 -0300

    Added basic State Machine

---
+++ Assets/Scripts/Ebac/StateMachine/StateMachineEditor.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FSMExample))]
+public class StateMachineEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        FSMExample fsm = (FSMExample)target;
+        EditorGUILayout.Space(30);
+        EditorGUILayout.LabelField("State Machine");
+        if (fsm.stateMachine == null) return;
+    }
+}
