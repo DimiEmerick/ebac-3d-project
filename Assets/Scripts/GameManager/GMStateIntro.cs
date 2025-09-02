f4694368f937fddfb010080d9be09f52f7fd5cb9
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-09-02 15:50:19 -0300

    Altered to English

--- Assets/Scripts/GameManager/GMStateIntro.cs
+++ Assets/Scripts/GameManager/GMStateIntro.cs
@@ -5,8 +5,12 @@ using Ebac.StateMachine;
 
 public class GMStateIntro : StateBase
 {
+    public override void OnStateEnter(object o = null)
+    {
+        Debug.Log("Entered Intro State");
+    }
     public override void OnStateExit()
     {
-        Debug.Log("Saiu do estado Intro");
+        Debug.Log("Exit Intro State");
     }
 }
