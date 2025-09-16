19d02e16303e12f43770365b316e817bf3208d48
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-09-16 16:27:47 -0300

    Added Enemy script and fixed prefab enemy faces

---
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Enemy
+{
+    public class EnemyBase : MonoBehaviour
+    {
+        public float startLife = 10f;
+        [SerializeField] private float _currentLife;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        protected virtual void Init()
+        {
+            ResetLife();
+            if(startWithBornAnimation)
+                BornAnimation();
+        }
+        protected void ResetLife()
+        {
+            _currentLife = startLife;
+        }
+
+        protected virtual void Kill() 
+        {
+            OnKill(); 
+        }
+
+        protected virtual void OnKill() 
+        {
+            Destroy(gameObject);
+        }
+
+        protected virtual void OnDamage(float f)
+        {
+            _currentLife -= f;
+            if(_currentLife <= 0)
+            {
+                Kill();
+            }
+        }
+
+        #region ANIMATION
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+        #endregion
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.T))
+            {
+                OnDamage(5f);
+            }
+        }
+    }
+}
