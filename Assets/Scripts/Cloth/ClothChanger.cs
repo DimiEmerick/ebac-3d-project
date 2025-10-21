8fbe12da7d8e4e2d8f17974b52bdc5927e3efa66
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-10-21 16:54:08 -0300

    Added cloth script

---
+++ Assets/Scripts/Cloth/ClothChanger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothChanger : MonoBehaviour
+{
+    public SkinnedMeshRenderer mesh;
+    public Texture2D texture;
+    public string shaderID = "_EmissionMap";
+
+    [NaughtyAttributes.Button]
+    private void ChangeTexture()
+    {
+        mesh.sharedMaterials[0].SetTexture(shaderID, texture);
+    }
+}
