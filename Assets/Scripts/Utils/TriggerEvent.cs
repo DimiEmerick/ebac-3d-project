640e72b583ba6c3518f179fa66aa576167ae3112
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-11-13 16:12:52 -0300

    More bug fixes

--- Assets/Scripts/Utils/TriggerEvent.cs
+++ Assets/Scripts/Utils/TriggerEvent.cs
@@ -6,9 +6,14 @@ using UnityEngine.Events;
 public class TriggerEvent : MonoBehaviour
 {
     public UnityEvent OnTriggerEvent;
+    public string tagToCompare;
 
     private void OnTriggerEnter(Collider other)
     {
-        OnTriggerEvent?.Invoke();
+        if(other.transform.CompareTag("Player"))
+        {
+            SaveManager.Instance.SaveLastLevel(0, 2);
+            OnTriggerEvent?.Invoke();
+        }
     }
 }
