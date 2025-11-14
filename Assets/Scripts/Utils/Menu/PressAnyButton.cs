dacd4a2884da1821bf73adfacb8320f1166a08d1
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-11-14 17:09:19 -0300

    Update PressAnyButton.cs

--- Assets/Scripts/Utils/Menu/PressAnyButton.cs
+++ Assets/Scripts/Utils/Menu/PressAnyButton.cs
@@ -1,11 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using TMPro;
+using DG.Tweening;
 
 public class PressAnyButton : MonoBehaviour
 {
     public GameObject mainMenu;
+    public TextMeshProUGUI text;
+    public float animationSize;
+    public float animationDuration;
+
+    private Tween _pulseTween;
+
+    private void Start()
+    {
+        _pulseTween = text.transform.DOScale(animationSize, animationDuration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.OutBack);
+    }
 
     private void Update()
     {
